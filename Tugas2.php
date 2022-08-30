0ac85f77884952429f707503b29f156cb5650cfa
DioMahardiansyah <diomahardiansyah@gmail.com>
2022-08-30 19:08:59 +0700

    Output pertemuan ke 2

---
+++ Tugas2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas2</title>
+</head>
+<body>
+    <center><h1>Tugas Pertemuan 2</h1></center>
+    
+</body>
+<?php
+    function perkenalan($nama, $umur, $alamat,)
+    {
+       echo "<br> Nama : ".$nama;
+       echo "<br> Umur : ".$umur;
+       echo "<br> Alamat : ".$alamat;
+
+    }
+    perkenalan("Dio Mahardiansyah Rajendra",20,"sidoarjo<br>");
+    perkenalan("Alfi Nur Danialin",21,"malang<br>");
+    perkenalan("Moh.Asep",19,"sampang<br>");
+?>
+</html>
